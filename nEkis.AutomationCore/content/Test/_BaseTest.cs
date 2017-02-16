c3ba175f9a0002df2e2812c59a5ab74b5c7c7cfa
David Nekovář <nekovard@gmail.com>
2017-02-16 10:21:07 +0100

    BaseTest and BasePage classes

---
+++ nEkis.AutomationCore/content/Test/_BaseTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace nEkis.AutomationCore.Test
+{
+    class BaseTest
+    {
+        [OneTimeSetUp]
+        public void BeforeAllTests()
+        {
+            Browser.CreateDriver(AvailableBrowsers.Chrome);
+        }
+
+        [SetUp]
+        public void BeforeTest()
+        {
+
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+
+        }
+
+        [OneTimeTearDown]
+        public void AfterAllTests()
+        {
+            Browser.QuitDriver();
+        }
+
+    }
+}
